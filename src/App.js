7978e3b38496764736eec2c15afcf71898b1254a
Markeisha <markeisha94@gmail.com>
2022-01-25 13:06:17 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 function App() {
+  const isLoggedIn = localStorage.getItem(token);
+  
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Friends Database</h1>
+      <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/">Friendlist</Link>
+          </li>
+          <li>
+            <Link to="/">AddFriend</Link>
+          </li>
+          <li>
+            <Link to="/">Logout</Link>
+          </li>
+        </ul>
+        {/* <Switch>
+          <PrivateRoute exact path="/protected" component={GasPrices} />
+          <Route path="/" component={Logout} />
+
+          <Route path="/" component={Login} />
+          <Route path="/" component={Login} /> 
+        </Switch>    */}
+      </div>
+    </Router>
+    // <div className="App">
+    //   <h2>Login</h2>
+    // </div>
   );
 }
 
