4ba3aea24a90fd3efa4a5977064018e16b416ddc
Markeisha <markeisha94@gmail.com>
2022-01-25 13:30:50 -0600

    built addFriend component

---
+++ src/components/AddFriend.js
@@ -0,0 +1,57 @@
+import React from "react";
+import axios from "axios";
+
+class AddFriend extends React.Component {
+    state = {
+        friend: {
+            name: '',
+            email: ''
+        }
+    };
+    handleChange = e => {
+        this.setState({
+            friend: {
+                ...this.state.friend,
+                [e.target.name]: e.target.value
+            }
+        })
+    };
+    submit = e => {
+        e.preventDefault()
+        const token = localStorage.getItem('token')
+
+        axios.post('', {
+            id: parseInt(Math.random()*100),
+            name: this.state.friend.name,
+            email: this.state.friend.email,
+            age: parseInt(Math.random()*100)
+        }, {
+            headers: {
+                authorization: token
+            }
+        })
+        .then(resp => {
+            this.props.history.push('/friends')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    };
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submit}>
+                    <label>Username</label>
+                    <input type='text' name='name' value={this.state.friend.username} onChange={this.handleChange}/>
+
+                    <label>Email</label>
+                    <input type='email' name='email' value={this.state.friend.password} onChange={this.handleChange}/>
+
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+};
+
+export default AddFriend;
